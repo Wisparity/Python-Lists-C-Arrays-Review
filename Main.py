7eaf2841be085dcf6c27dd36f9a8901d3f2fab73
Wisp <146360282+Wisparity@users.noreply.github.com>
2024-02-06 09:29:47 -0700

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,8 @@
+shopping = ["Bread","Cake","Grapes","Mangos"]
+shopping.remove("Bread")
+shopping.insert(3,'Oranges')
+print(shopping)
+if "apples" in shopping:
+    print("True")
+else:
+    print("false")
